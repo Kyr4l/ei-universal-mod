491493ecf84a0f48ea597f097ec99b4aeead3fbc
Kyr4l <alanemmanuel5@gmail.com>
2024-05-11 12:47:30 +0200

    added gitignore to prevent commiting useless junk

--- tools/_EI-BundledTools/compile_res.sh
+++ tools/_EI-BundledTools/compile_res.sh
@@ -5,15 +5,15 @@ res="res"
 # decompiled res folder
 rext="res_extracted"
 
-cd $rext
+cd $rext || exit
 
 for in in *_res; do
  out="${in%_res}.res"
- wine ../bin/eipacker.exe /pack $in
+ wine ../bin/eipacker.exe /pack "$in"
  echo "Done packing $in -> $out"
- rsync -r --remove-source-files ./$out ../"$res"/"$out"
+ rsync -r --remove-source-files ./"$out" ../"$res"/
  echo "RSync completed on $out"
- find -depth -type d -empty -delete
+ find . -depth -type d -empty -delete
  echo "Empty directories deleted"
 done
 
--- tools/_EI-BundledTools/extract_res.sh
+++ tools/_EI-BundledTools/extract_res.sh
@@ -5,15 +5,15 @@ res="res"
 # decompiled res folder
 rext="res_extracted"
 
-cd $res
+cd $res || exit
 
 for in in *.res; do
  out="${in%.res}_res"
- wine ../bin/eipacker.exe $in
+ wine ../bin/eipacker.exe "$in"
  echo "Done extracting $in -> $out"
- rsync -r --remove-source-files ./$out ../"$rext"/"$out"
+ rsync -r --remove-source-files ./"$out" ../"$rext"/
  echo "RSync completed on $out"
- find -depth -type d -empty -delete
+ find . -depth -type d -empty -delete
  echo "Empty directories deleted"
 done
 
