980e5af1efce3db3fe029e28a37383aab13f3c9e
Kyr4l <alanemmanuel5@gmail.com>
2024-05-17 15:34:56 +0200

    fixed the perma regen bug increased the attack/defense windows to reduce misses in melee

--- resources/lua/main.lua
+++ resources/lua/main.lua
@@ -10,8 +10,26 @@ function CalcRunningManaCost(unit_server_stats_addr, unit_server_addr, common_se
     local common_stats = UnitServerCommonStats(common_server_stats_addr)
     local mp = common_stats.mp
 
-    mp.curr = mp.curr - mp.max * (1 / 300)
+    mp.curr = mp.curr - mp.max * (1 / 375)
 end
+
+function RegenerateHp(common_server_stats_addr, unit_server_stats_addr, unit_addr) --> bool
+    local common_stats = UnitServerCommonStats(common_server_stats_addr)
+    local unit_stats   = UnitServerStats(unit_server_stats_addr)
+    local unit         = UnitServer(unit_addr)
+
+    local hp = common_stats.hp
+    if hp.curr >= hp.max then return false end
+
+    local hp_regen = hp.regen + hp.regen * unit_stats:perkModifier(PerkType.Vitality) * 0.01
+    local regen_spell = unit:spellEffect(SpellType.Regeneration)
+    if regen_spell > 0 then
+        hp_regen = hp_regen * regen_spell
+    end
+    unit_stats:heal(common_server_stats_addr, hp.max * hp_regen)
+    return true
+end
+
 
 -- Здесь можно заменить следующие функции (см. lua/default.lua для примера реализации):
 -- 1) GetModifiedDamageOnClient
